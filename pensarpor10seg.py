b73bba069bb379e34d190633931d715657e491e2
Yuri Lima <145035100+YuriLima66@users.noreply.github.com>
2024-03-06 16:42:45 -0300

    exercicios feitos hoje

---
+++ pensarpor10seg.py
@@ -0,0 +1,12 @@
+import time
+
+def PensarPor10Segundos():
+    print('pensando')
+    time.sleep(10)
+    print('Lembrei!')
+PensarPor10Segundos()    
+if 10>5:
+    print('10 é maior que 5')   
+    if 'hello'=='hello':
+        print('hello!')
+    
